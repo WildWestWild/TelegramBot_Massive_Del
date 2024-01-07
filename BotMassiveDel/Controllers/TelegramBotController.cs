fbb38eff4b154c69a8157ac6a78a4879c711a4ec
mkosikov <mJdwOVeBV4pW>
2024-01-08 01:26:46 +0300

    Add KeyboardHelper

--- BotMassiveDel/Controllers/TelegramBotController.cs
+++ BotMassiveDel/Controllers/TelegramBotController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.TelegramBot.Helpers;
+using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -22,38 +23,12 @@ namespace BotMassiveDel.Controllers
 
             Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
 
-            var replyKeyboard = new ReplyKeyboardMarkup(
-                new List<KeyboardButton[]>()
-                {
-                    new KeyboardButton[]
-                    {
-                        new("Покажи список \n (Имя списка)"),
-                        new("Создай новый список"),
-                    },
-                    new KeyboardButton[]
-                    {
-                        new("Добавь элемент"),
-                        new("Вычеркни элемент")
-                    },
-                    new KeyboardButton[]
-                    {
-                        new("Удали элемент"),
-                        new("Редактируй элемент")
-                    }
-                })
-            {
-                // автоматическое изменение размера клавиатуры, если не стоит true,
-                // тогда клавиатура растягивается чуть ли не до луны,
-                // проверить можете сами
-                ResizeKeyboard = true,
-            };
-
             // Echo received message text
             Message sentMessage = await botClient.SendTextMessageAsync(
                 chatId: chatId,
                 text: "You said:\n" + messageText,
                 cancellationToken: token,
-                replyMarkup: replyKeyboard);
+                replyMarkup: KeyboardHelper.GetKeyboard());
         }
     }
 }
